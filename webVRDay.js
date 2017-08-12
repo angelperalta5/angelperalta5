2585626e5e4a3fbbd44437719bb021d4636f9d43
Angel Peralta <30555900+angelperalta5@users.noreply.github.com>
2017-08-12 15:53:00 -0400

    Update webVRDay.js

--- webVRDay.js
+++ webVRDay.js
@@ -2,5 +2,6 @@ var vrHomePage = vRViewPlayer('#vr-homepage', {
   image: 'IMG_0444001.jpg',
   is_stereo: false,
   is_autopan_off: true,
+  is_yaw_only: true
 });
 window.addEventListener('load', vrHomePage);
